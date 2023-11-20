5ca9010686bd5b5f3707dded58d208c215dd617f
Sivani L R <109839211+sivani-l-r@users.noreply.github.com>
2023-11-20 10:12:35 +0530

    Create firebase.js

---
+++ Public/templates/firebase.js
@@ -0,0 +1,8 @@
+
+ 
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+  const firebaseConfig = {
+  
+  };
+
+  const app = initializeApp(firebaseConfig);
